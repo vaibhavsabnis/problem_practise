e532807a1d51641c8fa70956bb6e86c272f9cadc
vaibhavsabnis <vaibhav.sabnis@gmail.com>
2020-04-03 19:40:10 -0400

    Leet Code -3 challenge, Kadene's algo

---
+++ src/interview/prep/MaximumSubArray.java
@@ -0,0 +1,46 @@
+package interview.prep;
+/*
+ * Given an integer array nums, find the contiguous subarray (containing at least one number) 
+ * which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution 
+using the divide and conquer approach, which is more subtle.
+ */
+
+public class MaximumSubArray {
+	
+	public int maxSubArray(int[] nums) {
+		
+		int size = nums.length; 
+        int bestMax = Integer.MIN_VALUE, currentMax = 0;
+//        int st=0,en=0;
+  
+        for (int i = 0; i < size; i++) 
+        { 
+            currentMax = currentMax + nums[i]; 
+            
+            if (bestMax < currentMax) {
+                bestMax = currentMax;
+//                en=i;
+            }
+            if (currentMax < 0) { 
+                currentMax = 0;
+//                st=i;
+            }
+        } 
+//        System.out.println(st +" : "+en +" --> bestsum=" +bestMax);
+        return bestMax; 
+	}
+	
+	public static void main (String args[]) {
+		int nums[] = {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println(new MaximumSubArray().maxSubArray(nums));
+	}
+}
