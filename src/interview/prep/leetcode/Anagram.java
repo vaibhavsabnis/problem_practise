8f41c3ab8602fa565c242764379c2d957e8e0ca9
vaibhavsabnis <vaibhav.sabnis@gmail.com>
2020-04-05 15:44:39 -0400

    Without using Arrays.sort

--- src/interview/prep/leetcode/Anagram.java
+++ src/interview/prep/leetcode/Anagram.java
@@ -3,7 +3,8 @@ package interview.prep.leetcode;
 import java.util.Arrays;
 
 /*
- * Leetcode: Answer 242
+ * Leetcode: Question 242
+ * 
  * Given two strings s and t , write a function to determine if t is an anagram of s.
 
 Example 1:
@@ -22,7 +23,7 @@ What if the inputs contain unicode characters? How would you adapt your solution
  */
 
 public class Anagram {
-    public boolean isAnagram(String s, String t) {
+    public boolean isAnagram_solution1(String s, String t) {
     	if (s==null || t==null)
     			return false;
     	if (s.length() != t.length())
@@ -41,10 +42,31 @@ public class Anagram {
         return true;
     }
     
-    public static void main(String args[]) {
-//    	String s = "anagram", t="nagaram";
-    	String s = "car", t="arc";
+    public boolean isAnagram_solution2(String s, String t) {
+    	if (s==null || t==null)
+    			return false;
+    	if (s.length() != t.length())
+    			return false;
+
+    	int[] counts = new int[26];
+    	for (int i=0;i<s.length();i++) {
+    		counts[s.charAt(i) - 'a']++;
+    		counts[t.charAt(i) - 'a']--;
+    	}
     	
-    	System.out.println(new Anagram().isAnagram(s, t));
+    	for (int i:counts) {
+    		if (i!=0)
+    			return false;
+    	}
+    	
+        return true;
+    }
+    
+    public static void main(String args[]) {
+    	String s = "anagram", t="nagaram";
+//    	String s = "car", t="rat";
+    	
+    	System.out.println(new Anagram().isAnagram_solution1(s, t));
+    	System.out.println(new Anagram().isAnagram_solution2(s, t));
     }
 }
