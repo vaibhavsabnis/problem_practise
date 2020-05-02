e63c81b83c62c6f33e7538e99d37b9ba8bb4f134
vaibhavsabnis <vaibhav.sabnis@gmail.com>
2020-05-02 15:47:39 -0400

    May2nd Leetcode Solution

--- src/interview/prep/leetcode/JewelsAndStone.java
+++ src/interview/prep/leetcode/JewelsAndStone.java
@@ -1,7 +1,48 @@
 package interview.prep.leetcode;
 
-public class JewelsAndStone {
+import java.util.*;
 
+/*
+ * Jewels and Stones
+You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+
+The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+Example 1:
+
+Input: J = "aA", S = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: J = "z", S = "ZZ"
+Output: 0
+Note:
+
+S and J will consist of letters and have length at most 50.
+The characters in J are distinct.
+ */
+
+public class JewelsAndStone {
+	
+	public int numJewelsInStones(String J, String S) {
+		int howManyJewels=0;
+		
+		if(J == null || J.length()==0 || S ==null || S.length() ==0)
+			return 0;
+		
+		Set<Character> jewelSet = new HashSet<>();
+        for(char c: J.toCharArray()){
+        	jewelSet.add(c);
+        }
+        
+        for(int i=0; i<S.length();i++) {
+        	Character s = S.charAt(i);
+        	if (jewelSet.contains(s))
+        		howManyJewels++;
+        }
+        
+        return howManyJewels;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
