0d84c6128107c6f0a79436b7ae37f2b6f11b29b5
vaibhavsabnis <vaibhav.sabnis@gmail.com>
2020-08-01 08:19:39 -0400

    July 31st challenge

---
+++ src/interview/prep/leetcode/july20/ClimbingStairs.java
@@ -0,0 +1,54 @@
+package interview.prep.leetcode.july20;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Climbing Stairs
+
+Solution
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+ */
+public class ClimbingStairs {
+	public int climbStairs(int n) {        
+        Map<Integer,Integer> memo = new HashMap<>(); 
+        return helper (n,0,memo);        
+    }
+    private int helper(int n, int totalSteps, Map<Integer,Integer> memo){
+        if(n < 0)
+            return totalSteps;
+        if (n ==0 || n ==1 || n==2)
+            return totalSteps + n;
+        
+        if(memo.get(n)!=null)
+            return memo.get(n); 
+        
+        totalSteps = helper(n-1,totalSteps,memo) + helper(n-2,totalSteps,memo); 
+        memo.put(n, totalSteps);
+        
+        return totalSteps;
+    }
+}
