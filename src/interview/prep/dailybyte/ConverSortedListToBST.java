6b95bdb4c82549d551838095c9192f7db6ed626b
vaibhavsabnis <vaibhav.sabnis@gmail.com>
2021-04-02 15:24:25 -0400

    Daily Byte Practise

--- src/interview/prep/dailybyte/ConverSortedListToBST.java
+++ src/interview/prep/dailybyte/ConverSortedListToBST.java
@@ -5,7 +5,48 @@ import interview.prep.leetcode.myutils.TreeNode;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+109. Convert Sorted List to Binary Search Tree
+Medium
 
+2789
+
+93
+
+Add to List
+
+Share
+Given the head of a singly linked list where elements are sorted in ascending order, convert it to a height balanced BST.
+
+For this problem, a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+
+
+Example 1:
+
+
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+Example 2:
+
+Input: head = []
+Output: []
+Example 3:
+
+Input: head = [0]
+Output: [0]
+Example 4:
+
+Input: head = [1,3]
+Output: [3,1]
+
+
+Constraints:
+
+The number of nodes in head is in the range [0, 2 * 104].
+-10^5 <= Node.val <= 10^5
+ */
 public class ConverSortedListToBST {
     //TC: O(N), SC: O(N) - N = number of listnodes in the incoming list
     public TreeNode sortedListToBST(ListNode head) {
